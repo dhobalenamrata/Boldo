cf2eaccb810f3eb439e0d13fefb3b116f4d21a5b
Namrata Dhobale <121574685+dhobalenamrata@users.noreply.github.com>
2024-02-05 12:47:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        this.classList.toggle("Active");
+        this.classList.toggle("active-accordion");
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
